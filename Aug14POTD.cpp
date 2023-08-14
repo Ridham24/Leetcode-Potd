72e062b8fefd9f0155cff2bd89fdcda4dbf54642
Ridham24 <ridhamgoel03@gmail.com>
2023-08-14 20:12:53 +0530

    Aug14 POTD

---
+++ Aug14POTD.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> p;
+        for(int i=0;i<k;i++)
+        p.push(nums[i]);
+        for(int i=k;i<nums.size();i++)
+        {
+            if(nums[i]>p.top())
+            {
+                p.pop();
+                p.push(nums[i]);
+            }
+        }
+        return p.top();
+    }
+};
+int main(){
+
+return 0;
+}
