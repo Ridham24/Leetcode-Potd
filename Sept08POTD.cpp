d3fc044c82287cbb54501ecdc50e4ef004f8f671
Ridham24 <ridhamgoel03@gmail.com>
2023-09-09 00:10:22 +0530

    Sept 08 POTD

---
+++ Sept08POTD.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> generate(int n) {
+        vector<vector<int>> v;
+        vector<int> k;
+        v.push_back({1});
+        n--;
+        int s=0;
+        int j=1;
+        while(j<=n)
+        {
+            k.push_back(1);
+            for(int i=1;i<j;i++)
+            {
+                s=v[j-1][i]+v[j-1][i-1];
+                k.push_back(s);
+            }
+            k.push_back(1);
+            v.push_back(k);
+            k.clear();
+            j++;
+        }
+        return v;
+    }
+int main(){
+
+return 0;
+}
