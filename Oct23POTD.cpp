9f5f6e82137f82fce1b8b1111d329fb2c5d448c9
Ridham24 <ridhamgoel03@gmail.com>
2023-10-23 19:49:17 +0530

    Oct 23 POTD

---
+++ Oct23POTD.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPowerOfFour(int n) {
+        if(n<=0)
+        return false;
+        if((n&(n-1))==0&&n%3==1)
+        return true;
+        return false;
+    }
+int main(){
+
+return 0;
+}
