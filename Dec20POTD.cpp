8a6cd19eed0a15f30be606b491a2063c592b4816
Ridham24 <ridhamgoel03@gmail.com>
2023-12-20 14:44:54 +0530

    Dec 20 POTD

---
+++ Dec20POTD.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int a=prices[0]+prices[1];
+        if(money>=a)
+            return money-a;
+        else
+            return money;
+    }
+};
